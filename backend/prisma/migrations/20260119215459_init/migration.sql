3b25e464083f6f5fe9927dd2616824fa702a57f0
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-01-20 01:06:26 +0300

    feat(backend): Миграции

--- backend/prisma/migrations/20260119215459_init/migration.sql
+++ backend/prisma/migrations/20260119215459_init/migration.sql
@@ -1,16 +1,29 @@
 -- CreateTable
 CREATE TABLE "organizations" (
     "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
     "inn" TEXT NOT NULL,
     "ogrn" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "address" TEXT NOT NULL,
     "description" TEXT,
     "logo" TEXT,
+    "sort_index" INTEGER NOT NULL DEFAULT 500,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "organizations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "resource_categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "label" TEXT NOT NULL,
+    "sort_index" INTEGER NOT NULL DEFAULT 500,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "resource_categories_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -20,6 +33,7 @@ CREATE TABLE "resources" (
     "contacts" TEXT,
     "description" TEXT,
     "organization_id" TEXT NOT NULL,
+    "category_id" TEXT NOT NULL,
     "is_private" BOOLEAN NOT NULL DEFAULT false,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
@@ -46,6 +60,7 @@ CREATE TABLE "users" (
     "last_name" TEXT NOT NULL,
     "patronymic" TEXT,
     "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
     "is_banned" BOOLEAN NOT NULL DEFAULT false,
     "role_id" TEXT NOT NULL,
@@ -54,6 +69,26 @@ CREATE TABLE "users" (
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "alerts" (
+    "id" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "alerts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organizations_inn_key" ON "organizations"("inn");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organizations_ogrn_key" ON "organizations"("ogrn");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "resource_categories_name_key" ON "resource_categories"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "resources_organization_id_name_key" ON "resources"("organization_id", "name");
@@ -67,11 +102,11 @@ CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 -- CreateIndex
 CREATE INDEX "users_email_idx" ON "users"("email");
 
--- CreateIndex
-CREATE INDEX "users_phone_idx" ON "users"("phone");
-
 -- AddForeignKey
 ALTER TABLE "resources" ADD CONSTRAINT "resources_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "resources" ADD CONSTRAINT "resources_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "resource_categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "users" ADD CONSTRAINT "users_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
